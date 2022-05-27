a8991ffa88cd6b25f380b41900d1ba3e1fc896fa
hugosenari <hugosenari@gmail.com>
2022-05-27 18:22:25 -0300

    chore: re index illwave

--- flake.nix
+++ flake.nix
@@ -23,6 +23,22 @@
   inputs."illwave-0_1_0".inputs.nixpkgs.follows = "nixpkgs";
   inputs."illwave-0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
   
+  inputs."illwave-0_2_0".dir   = "0_2_0";
+  inputs."illwave-0_2_0".owner = "nim-nix-pkgs";
+  inputs."illwave-0_2_0".ref   = "master";
+  inputs."illwave-0_2_0".repo  = "illwave";
+  inputs."illwave-0_2_0".type  = "github";
+  inputs."illwave-0_2_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."illwave-0_2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."illwave-0_3_0".dir   = "0_3_0";
+  inputs."illwave-0_3_0".owner = "nim-nix-pkgs";
+  inputs."illwave-0_3_0".ref   = "master";
+  inputs."illwave-0_3_0".repo  = "illwave";
+  inputs."illwave-0_3_0".type  = "github";
+  inputs."illwave-0_3_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."illwave-0_3_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
